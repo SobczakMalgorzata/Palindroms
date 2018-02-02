ac0624005802e41853044b52a675525c85193f64
Małgorzata Sobczak <sobczakmalgorz@gmail.com>
2018-02-02 14:33:00 +0100

    Initial form

---
+++ ConsoleApp1/Palindroms.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Palindroms
+    {
+        public Palindroms() { }
+        public List<string> GetPalindroms(string s, int i)
+        {
+            List<string> list = new List<string>();
+
+            for (int k = 0; k < s.Length - i; k++)
+            {
+                char[] word = new char[i];
+                for (int l = 0; l<i;l++)
+                {
+                    word[l] = s[k+l];
+                }
+                bool palindrom = true;
+
+
+                for (int j = 0; (i - (2 * j)) > 1; j++)
+                {
+                    if (palindrom && word[j] == word[i - j - 1])
+                    {
+                        palindrom = true;
+                    }
+                    else
+                    {
+                        palindrom = false;
+                    }
+                }
+                if (palindrom)
+                {
+                    list.Add(new string(word));
+                }
+            }
+            return list;
+        }
+
+
+        public string GetSmalest(List<string> list)
+        {
+            int min = Int32.Parse(list[0]);
+            string word = list[0];
+            foreach (string s in list)
+            {
+                int next = Int32.Parse(s);
+                if (next < min)
+                {
+                    min = next;
+                    word = s;
+                }
+            }
+            return word;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Constant c = new Constant();
+            string t = c.Pi();
+            Console.Out.WriteLine("Our String: ");
+            Console.Out.WriteLine(t);
+            Console.Out.Write("Write length of palindrom: ");
+            Palindroms p = new Palindroms();
+            int l;
+            if (Int32.TryParse(Console.In.ReadLine(), out int o))
+            {
+                l = o;
+            }
+            else
+            {
+                l = 5;
+            }
+            List<string> list = p.GetPalindroms(t, l);
+            foreach (string x in list)
+            {
+                Console.Out.WriteLine((list.IndexOf(x)+ 1) +": "+ x);
+            }
+            if (list.Count > 0)
+            {
+                Console.Out.WriteLine("Smallest: " + p.GetSmalest(list));
+            }
+            Console.In.Read();
         }
+
+        
     }
 }
